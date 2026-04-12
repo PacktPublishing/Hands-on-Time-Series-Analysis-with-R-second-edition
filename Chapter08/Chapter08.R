ae9bd3f90f2aef614cf587c72a735e47ad7a9038
Shuen Mei <smei1@yahoo.com>
2026-04-12 16:11:57 -0500

    initial commit for Chapter08

--- Chapter08/Chapter08.R
+++ Chapter08/Chapter08.R
@@ -1,3 +1,13 @@
+# Hands-on Time Series Analysis with R second Edition
+# Chapter 08 Code
+#
+
+
+# Check if the forecast package is already installed
+if (!requireNamespace("forecast", quietly = TRUE)) {
+  # If not installed, install it
+  install.packages("forecast")
+}
 # Load the forecast package
 library(forecast)
 
@@ -22,12 +32,19 @@ plot(tbats_forecast, main="TBATS Forecast for AirPassengers", ylab="Number of Pa
 # Calculate the accuracy of the TBATS model
 accuracy(tbats_forecast)
 
+
 # Adjusting the number of Fourier terms in the TBATS model
+# Using seasonal period of 12 (month) 
+# TBATS automatically picks k for each period
 tbats_model <- tbats(AirPassengers, use.parallel=TRUE, num.cores=NULL, seasonal.periods=c(12))
+# Print the k value for the periods 
+print(tbats_model$k.vector)
 
 # Applying a Box-Cox transformation in the TBATS model
 lambda <- BoxCox.lambda(AirPassengers)
 tbats_model <- tbats(AirPassengers, lambda=lambda)
+print(lambda)
+
 
 # Including ARMA errors in the TBATS model
 tbats_model <- tbats(AirPassengers, use.arma.errors=TRUE)
@@ -37,3 +54,4 @@ tbats_model <- tbats(AirPassengers, seasonal.periods=c(12, 6))
 
 # Fine-tuning the TBATS model parameters
 tbats_model <- tbats(AirPassengers, use.trend=TRUE, use.damped.trend=TRUE, seasonal.periods=c(12), use.parallel=TRUE)
+
